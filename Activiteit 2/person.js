55b3634b9fdc5279f275551ade189aedaffec797
Davey Hoogland <greenbluerup@gmail.com>
2015-08-31 09:30:48 +0200

    Init

---
+++ Activiteit 2/person.js
@@ -0,0 +1,15 @@
+/*********************************************************************
+*                                                                    *
+*  Project   : Module DHTML                                          *
+*  File      : person.js  (stripped versie)                          *
+*  Browser   :                                                       *
+*  Auteur    : Kasper Kombrink, Koos de Wit                          *
+*  Datum     : sept 2003                                             *
+*  Versie    : 1                                                     *
+*                                                                    *
+*********************************************************************/
+
+function askInformation()
+{
+	// this function has to be coded by the student
+}
